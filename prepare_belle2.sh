673693575a1427aac183d4684c299141245cd960
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2010-12-16 16:59:32 +0000

    patch and wget packages added
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/tools@1046 b38c6c8f-bee8-465d-8448-9abe6915d496

--- prepare_belle2.sh
+++ prepare_belle2.sh
@@ -2,20 +2,32 @@
 
 if [ -f /etc/lsb-release ]; then
   # Ubuntu
-  sudo apt-get install subversion make gcc g++ gfortran binutils python-dev libxml2-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev
+  sudo apt-get install subversion make gcc g++ gfortran binutils patch wget python-dev libxml2-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev
 
 elif [ -f /etc/debian_version ]; then
   # Debian
-  su -c "apt-get install subversion make gcc g++ gfortran binutils python-dev libxml2-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev openssl-dev"
+  su -c "apt-get install subversion make gcc g++ gfortran binutils patch wget python-dev libxml2-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev openssl-dev"
 
 else
   if [ ! -f /etc/redhat-release ]; then
     echo "Unknown linux distribution. Trying installation with yum..."
   fi
   # RH, SL, CentOS
-  su -c "yum install subversion make gcc gcc-c++ gcc-gfortran binutils python-devel libxml2-devel libX11-devel libXpm-devel libXft-devel libXext-devel openssl-devel"
+  su -c "yum install subversion make gcc gcc-c++ gcc-gfortran binutils patch wget python-devel libxml2-devel libX11-devel libXpm-devel libXft-devel libXext-devel openssl-devel"
 fi
 
+
+# set up svn if it is installed in the home directory
+if [ -d ${HOME}/subversion ]; then
+  export PATH=${HOME}/subversion/bin:$PATH
+  if [ -n "${LD_LIBRARY_PATH}" ]; then
+    export LD_LIBRARY_PATH=${HOME}/subversion/lib:${LD_LIBRARY_PATH}
+  else
+    export LD_LIBRARY_PATH=${HOME}/subversion/lib
+  fi
+fi
+
+# check svn version and download and install a new version if the available one is too old
 SVN_MAJOR_VERSION=`svn --version | head -1 | awk '{print $3}' | awk -F . '{print $1}'`
 SVN_MINOR_VERSION=`svn --version | head -1 | awk '{print $3}' | awk -F . '{print $2}'`
 if [ ${SVN_MAJOR_VERSION} -lt 2 ]; then
