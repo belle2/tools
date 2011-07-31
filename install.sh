d490f511aa377d947226fbd650c1082b1c2cd1d8
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2011-07-31 07:23:13 +0000

    use svn export instead of svn co for the installation of astyle and scons
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/tools@1844 b38c6c8f-bee8-465d-8448-9abe6915d496

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 # astyle
 if [ ! -f ${DIR}/astyle ]; then
   cd ${DIR}/src
-  svn co -r56 https://astyle.svn.sourceforge.net/svnroot/astyle/tags/1.23/AStyle astyle
+  svn export -r56 https://astyle.svn.sourceforge.net/svnroot/astyle/tags/1.23/AStyle astyle
   cd astyle/build/gcc
   make
  cp bin/astyle ${DIR}
@@ -18,7 +18,7 @@ fi
 # scons
 if [ ! -f ${DIR}/scons ]; then
   cd ${DIR}/src
-  svn co --username guest --password guest --non-interactive -r4725 http://scons.tigris.org/svn/scons/tags/1.3.0 scons
+  svn export --username guest --password guest --non-interactive -r4725 http://scons.tigris.org/svn/scons/tags/1.3.0 scons
   cd scons
   python bootstrap.py build/scons
   cd build/scons
