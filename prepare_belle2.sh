a266637e47171420cbf4c7d4abaa7d09b799d005
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2012-12-07 15:05:16 +0000

    check for optional packages required for the event display
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/tools@4380 b38c6c8f-bee8-465d-8448-9abe6915d496

--- prepare_belle2.sh
+++ prepare_belle2.sh
@@ -18,6 +18,7 @@ if [ `uname` = Darwin ]; then
     exit 1
   fi
   PACKAGES="wget"
+  OPTIONALS=""
   CHECK_CMD="dpkg -s"
   SU_CMD="sudo"
   INSTALL_CMD="fink install"
@@ -25,6 +26,7 @@ if [ `uname` = Darwin ]; then
 elif [ -f /etc/lsb-release ]; then
   # Ubuntu
   PACKAGES="subversion make gcc g++ gfortran binutils patch wget python-dev libxml2-dev dpkg-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev libncurses5-dev libreadline-dev lsb-release"
+  OPTIONALS="libglu1-mesa-dev libglew1.5-dev"
   CHECK_CMD="dpkg -s"
   SU_CMD="sudo"
   INSTALL_CMD="apt-get install"
@@ -32,6 +34,7 @@ elif [ -f /etc/lsb-release ]; then
 elif [ -f /etc/debian_version ]; then
   # Debian
   PACKAGES="subversion make gcc g++ gfortran binutils patch wget python-dev libxml2-dev dpkg-dev libx11-dev libxpm-dev libxft-dev libxext-dev libbz2-dev libssl-dev libncurses5-dev libreadline-dev lsb-release"
+  OPTIONALS="libglu1-mesa-dev libglew1.5-dev"
   CHECK_CMD="dpkg -s"
   SU_CMD="su -c"
   INSTALL_CMD="apt-get install"
@@ -39,6 +42,7 @@ elif [ -f /etc/debian_version ]; then
 elif [ -f /etc/SuSE-release ]; then
   # OpenSUSE
   PACKAGES="subversion make gcc gcc-c++ libgfortran45 binutils patch wget python-devel libxml2-devel xorg-x11-libX11-devel xorg-x11-libXpm-devel xorg-x11-libXext-devel libbz2-devel ncurses-devel readline-devel" 
+  OPTIONALS="mesa-libGL-devel glew-devel"
   CHECK_CMD="rpm -q"
   SU_CMD="su -c"
   INSTALL_CMD="yum install"
@@ -49,6 +53,7 @@ else
   fi
   # RH, SL, CentOS
   PACKAGES="subversion make gcc gcc-c++ gcc-gfortran binutils patch wget python-devel libxml2-devel libX11-devel libXpm-devel libXft-devel libXext-devel bzip2-devel openssl-devel ncurses-devel readline-devel"
+  OPTIONALS="mesa-libGL-devel glew-devel"
   CHECK_CMD="rpm -q"
   SU_CMD="su -c"
   INSTALL_CMD="yum install"
@@ -62,6 +67,15 @@ for PACKAGE in ${PACKAGES}; do
   ${CHECK_CMD} ${PACKAGE} &> /dev/null
   if [ "$?" != 0 ]; then
     MISSING_PACKAGES="${MISSING_PACKAGES} ${PACKAGE}"
+  fi
+done
+
+# check for missing optional packages
+MISSING_OPTIONALS=""
+for PACKAGE in ${OPTIONALS}; do
+  ${CHECK_CMD} ${PACKAGE} &> /dev/null
+  if [ "$?" != 0 ]; then
+    MISSING_OPTIONALS="${MISSING_OPTIONALS} ${PACKAGE}"
   fi
 done
 
@@ -86,6 +100,37 @@ You will need root access to run this command.
   if [ "$REPLY" = "y" ]; then
     if [ "${SU_CMD}" != "sudo" ]; then
       ${SU_CMD} "${INSTALL_CMD}${MISSING_PACKAGES}"
+    else
+      ${SU_CMD} ${INSTALL_MISSING}
+    fi
+    if [ "$?" != 0 ]; then
+      exit 1
+    fi
+  else
+    exit 1
+  fi
+fi
+
+# ask the user about the optional packages
+if [ -n "${MISSING_OPTIONALS}" ]; then
+  TEXT="now"
+  INSTALL_MISSING=${INSTALL_CMD}${MISSING_OPTIONALS}
+  if [ "${SU_CMD}" != "sudo" ]; then
+    INSTALL_MISSING=\"${INSTALL_MISSING}\"
+  fi
+  echo "The following optional packages (required to build the event display) are not installed:${MISSING_OPTIONALS}
+
+You can install them with the following command:
+
+  ${SU_CMD} ${INSTALL_MISSING}
+
+You will need root access to run this command.
+"
+  read -p "Would you like to execute it now (y/n)? " -n 1 REPLY 
+  echo
+  if [ "$REPLY" = "y" ]; then
+    if [ "${SU_CMD}" != "sudo" ]; then
+      ${SU_CMD} "${INSTALL_CMD}${MISSING_OPTIONALS}"
     else
       ${SU_CMD} ${INSTALL_MISSING}
     fi
