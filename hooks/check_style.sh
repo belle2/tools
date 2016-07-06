179ab65e796967b9e056a5d93cc7e104d3a0b2ad
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2016-07-06 18:58:47 +0200

    Updated pre-commit and check_style.sh

--- hooks/check_style.sh
+++ hooks/check_style.sh
@@ -1,34 +1,22 @@
 #!/usr/bin/env bash
 
-# To check the files to be commited we need a temporary dir as there might
-# be differences between the file in the index and the file on disk
-TMPDIR=`mktemp -d`
-if [ $? -ne 0 ]; then
-  echo "Problem creating temp dir, aborting commit."
-  exit 2
-fi
+# Go to temp dir containing the files to be committed
+cd $1
 
-# Find out which files were modified
-FILES_MODIFIED=`git diff --cached --name-only`
-# Place a copy of the versions to be commited in the temp dir
-git checkout-index --prefix=$TMPDIR/ -- $FILES_MODIFIED
-# Check all the files in the temp dir and write which ones failed the check
+# Loop over all files and check their style
 FAIL=0
-for FILE in $FILES_MODIFIED; do
-  # Suppressing output since the path would be wrong anyways
-  checkstyle "$TMPDIR/$FILE" > /dev/null
+for FILE in `find . -type f`; do
+  # exclude trg package
+  if [ `echo ${FILE} | cut -c 1-6` = "./trg/" ]; then
+    continue
+  fi
+  checkstyle "$FILE" > /dev/null
   if [ $? -ne 0 ]; then
-    echo "checkstyle failed for $FILE"
+    echo "checkstyle failed for "`echo $FILE | cut -c 3-`
     FAIL=1
   fi
 done
 
-# If at least one fails, abort commit
-if [ $FAIL -ne 0 ]; then
-  echo "Commit aborted, please run 'fixstyle' on the files listed above and 'git add' them to your commit again."
-fi
-
-# Clean up the temp dir
-rm -r $TMPDIR
+# Fail check if at least one fails
 exit $FAIL
 
