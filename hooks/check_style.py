1595d220c47247839edb1305e0b0f73ee484e488
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2018-05-05 23:21:19 +0200

    hook adjusted to new names

--- hooks/check_style.py
+++ hooks/check_style.py
@@ -16,14 +16,14 @@ for root, dirs, files in os.walk('.'):
         if len([exception for exception in exceptions if file_name.startswith(exception)]) > 0:
             continue
         if os.path.splitext(file_name)[1] in ['.h', '.cc', '.py']:
-            if subprocess.call(['checkstyle', file_name], stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w')) != 0:
+            if subprocess.call(['b2code-style-check', file_name], stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w')) != 0:
                 failed_files.append(file_name)
-            
+
 # print instructions in case of failed test
 if len(failed_files) > 0:
     print('\nThe following files do not comply with the style rules:')
     for failed_file in failed_files:
         print((' %s' % failed_file))
-    print("=> Run the 'fixstyle' tool on the files listed above and 'git add' them to your commit again.")
+    print("=> Run the 'b2code-style-fix' tool on the files listed above and 'git add' them to your commit again.")
     print("   (And make sure you have the latest version in your tools directory.)")
     sys.exit(1)
