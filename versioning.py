cf48137c8c4e4df27b3c69a382afe736e9cfbe5f
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2018-07-25 23:22:52 +0200

    versioning.py added to get versioning.py from git repository

---
+++ versioning.py
@@ -0,0 +1,51 @@
+
+"""Management of software versions and global tags via versioning git repository
+"""
+
+import sys, os, subprocess
+
+
+def get_remote_versioning(repository):
+    """Get versioning.py from central git repository"""
+
+    command = ['git', 'archive', '--remote=' + repository, 'HEAD', 'versioning.py']
+    git = subprocess.Popen(command, stdout=subprocess.PIPE)
+    tar = subprocess.Popen(['tar', '-xO', 'versioning.py'], stdin=git.stdout, stdout=subprocess.PIPE)
+    git.stdout.close() 
+    versioning = tar.communicate()[0]
+    if tar.returncode == 0:
+        return versioning
+    else:
+        return None
+
+
+def get_local_versioning(directory):
+    """Get versioning.py from local repository clone"""
+
+    filename = os.path.join(directory, 'versioning', 'versioning.py')
+    if os.path.isfile(filename):
+        return open(filename).read()
+    else:
+        return None
+
+
+# try different sources of versioning.py
+versioning = get_remote_versioning(os.environ['BELLE2_GIT_SERVER'] + '/b2/versioning.git')
+if versioning is None:
+    for directory in ['/cvmfs/belle.cern.ch', os.environ['VO_BELLE2_SW_DIR']]:
+        versioning = get_local_versioning(directory)
+        if versioning is not None:
+            break
+
+# if found, execute versioning.py, else define functions returning None
+if versioning is not None:
+    exec(versioning)
+else:
+    sys.stderr.write('Warning: Could not get versioning information\n')
+    def supported_release(release):
+        return None
+    def recommended_global_tags(release, mc=False, analysis=True, input_tags=[]):
+        return None
+    def upload_global_tag(task):
+        return None
+
