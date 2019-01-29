07c549bb502ed6fbda2516b9e8ecaa5aac3adfe3
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2019-01-29 22:31:37 +0100

    tools with deprecated names removed

--- setup_belle2_ana.sh
+++
@@ -1,7 +0,0 @@
-# setup Belle II software tools
-. `python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' $(dirname ${BASH_SOURCE:-$0})`/setup_belle2.sh
-
-# setup the analysis if the current directory contains Belle II analysis code
-if [ -f .analysis ]; then
-  setupana $*
-fi
