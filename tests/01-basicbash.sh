696e2158306f856b3613dc17fe6f446b04f30f8b
Martin Ritter <martin.ritter@desy.de>
2020-07-21 11:05:36 +0200

    Also test that b2code-option works
    especially when sourcing directly b2setup.sh with a relative path.

--- tests/01-basicbash.sh
+++ tests/01-basicbash.sh
@@ -12,4 +12,21 @@ if [ -d "${VO_BELLE2_SW_DIR}/releases/${RECOMMENDED}" ]; then
     b2setup ${RECOMMENDED}
     echo "Trying to run basf2 --info"
     basf2 --info
+
+    # set the code option to something
+    b2code-option clang
+    if ! [ "$BELLE2_OPTION" == "clang" ]; then
+        echo "Failed to set compiler option to clang"
+        exit 1
+    fi
 fi
+
+# echo execute at least one of the functions intended to modify the environment
+b2code-option --help
+
+# now make sure everything also works when sourcing from a relative directory after changing it
+echo "Try again relative"
+pushd $BELLE2_TOOLS/tests
+source ../b2setup.sh
+pushd $(mktemp -d)
+b2code-option --help
