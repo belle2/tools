56cafc4a3f1dd1803fdf29d60beeabf8756d95bc
Moritz Bauer <moritz.bauer@desy.de>
2022-09-22 12:40:31 +0200

    Simplify arguments passed to generate_documentation, make shellcheck happy and make rst consistent.

--- doc/generate_documentation.sh
+++ doc/generate_documentation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# This script generates a provided-scripts.rst file from the --documentation option in all Belle II bash scripts.
+# The argument specifies the relative directory provided-scripts.rst should be written to.
 
+target=$1/provided-scripts.rst
 
-cat << EOF > $1 
+cat << EOF > "$target"
 Provided Scripts
 ----------------
 
@@ -15,21 +18,21 @@ EOF
 for file in b2analysis-create b2analysis-get b2install-release b2install-externals b2install-data ; do
 
   # Check it's a file
-  if [ -f $2/"$file" ]; then
+  if [ -f "${BELLE2_TOOLS}/$file" ]; then
     # Check it's a shell script and not something else
-    if [ "$(head -n 1 $2/$file)" = "#!/bin/bash" ]; then
-        $2/$file --documentation >> $1 
+    if [ "$(head -n 1 "${BELLE2_TOOLS}/$file")" = "#!/bin/bash" ]; then
+        "${BELLE2_TOOLS}/$file" --documentation >> "$target"
     else
-        echo "$2/$file is not a bash script"
+        echo "${BELLE2_TOOLS}/$file is not a bash script"
         exit 1
     fi
   else
-    echo "$2/$file is not a bash script"
+    echo "${BELLE2_TOOLS}/$file is not a bash script"
     exit 1
   fi
 done
 
-cat << EOF >> $1 
+cat << EOF > "$target"
 
 For developers
 ++++++++++++++
@@ -39,16 +42,16 @@ EOF
 for file in b2code-create b2code-style-check b2code-style-fix b2code-clean b2code-package-list b2code-package-add b2code-package-tag b2install-prepare ; do
 
   # Check it's a file
-  if [ -f $2/"$file" ]; then
+  if [ -f "${BELLE2_TOOLS}/$file" ]; then
     # Check it's a shell script and not something else
-    if [ "$(head -n 1 $2/$file)" = "#!/bin/bash" ]; then
-        $2/$file --documentation >> $1 
+    if [ "$(head -n 1 "${BELLE2_TOOLS}/$file")" = "#!/bin/bash" ]; then
+        "${BELLE2_TOOLS}/$file" --documentation >> "$target"
     else
-        echo "$2/$file is not a bash script"
+        echo "${BELLE2_TOOLS}/$file is not a bash script"
         exit 1
     fi
   else
-    echo "$2/$file is not a bash script"
+    echo "${BELLE2_TOOLS}/$file is not a bash script"
     exit 1
   fi
 done
