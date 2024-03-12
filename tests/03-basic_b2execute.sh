01d20aed5afcdf6df6611b17906b13ca786d34ab
Giacomo De Pietro <giacomo.pietro@kit.edu>
2024-03-12 12:10:40 +0100

    Improve how the output file is defined/handled

--- tests/03-basic_b2execute.sh
+++ tests/03-basic_b2execute.sh
@@ -6,11 +6,13 @@ echo "Sourcing tools ..."
 source ${BELLE2_TOOLS}/b2setup
 echo "Getting recommended release ..."
 RECOMMENDED=$(b2help-releases)
+# Define the output file
+OUTPUT_FILE=${BELLE2_TOOLS}/tests/test_output_file.root
 # Run the test steering file with b2execute
 echo "Executing test_steering_file.py with ${RECOMMENDED} and a simple option via b2execute..."
-b2execute ${RECOMMENDED} ${BELLE2_TOOLS}/tests/test_steering_file.py -n 10 -o test_output_file.root
+b2execute ${RECOMMENDED} ${BELLE2_TOOLS}/tests/test_steering_file.py -n 10 -o ${OUTPUT_FILE}
 # Check if also b2execute -x works
 echo "Executing b2file-metadata-show with ${RECOMMENDED} and a simple option via b2execute..."
-b2execute -x b2file-metadata-show ${RECOMMENDED} test_output_file.root -a
+b2execute -x b2file-metadata-show ${RECOMMENDED} ${OUTPUT_FILE} -a
 # Clean the output file(s)
-rm -rf test_output_file.root
+rm -rf ${OUTPUT_FILE}
