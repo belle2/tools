617c6e6323fd9a4a14b4325f66e1ef668e2e3840
Giacomo De Pietro <giacomo.pietro@kit.edu>
2024-03-12 11:12:47 +0100

    Test also b2xecute -x

--- tests/03-basic_b2execute.sh
+++ tests/03-basic_b2execute.sh
@@ -7,5 +7,10 @@ source ${BELLE2_TOOLS}/b2setup
 echo "Getting recommended release ..."
 RECOMMENDED=$(b2help-releases)
 # Run the test steering file with b2execute
-echo "Executing test_steering_file.py with ${RECOMMENDED} and a simple option..."
-b2execute ${RECOMMENDED} ${BELLE2_TOOLS}/tests/test_steering_file.py -n 10
+echo "Executing test_steering_file.py with ${RECOMMENDED} and a simple option via b2execute..."
+b2execute ${RECOMMENDED} ${BELLE2_TOOLS}/tests/test_steering_file.py -n 10 -o test_output_file.root
+# Check if also b2execute -x works
+echo "Executing b2file-metadata-show with ${RECOMMENDED} and a simple option via b2execute..."
+b2execute -x b2file-metadata-show ${RECOMMENDED} test_output_file.root -a
+# Clean the output file(s)
+rm -rf test_output_file.root
